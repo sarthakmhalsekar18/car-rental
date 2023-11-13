a2d08be95017256cbca4b7d4fccfa85fa6c081ab
sarthakmhalsekar18 <116620219+sarthakmhalsekar18@users.noreply.github.com>
2023-11-13 11:25:22 +0530

    Update about.php

--- about.php
+++ about.php
@@ -20,7 +20,7 @@
         <div class="bx bx-menu" id="menu-icon"></div>
     
         <ul class="navbar">
-            <li><a href="home.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="ride.php">Ride</a></li>
             <li><a href="services.php">Service</a></li>
             <li><a href="about.php">About</a></li>
